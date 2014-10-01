370a52f149ab6d858778859b862da9eedd37ded3
DoubleMalt <christoph@web.crofting.com>
2014-10-01 10:58:44 +0200

    re activate container creation

--- plugins/users.py
+++ plugins/users.py
@@ -31,10 +31,10 @@ def handle(event):
             with open(settings.PORT_ASSIGNMENT_FILE_LOCATION, 'w') as port_assignments_file:
                 json.dump(port_assignments, port_assignments_file) #FIXME make atomic
 
-        # container = c.create_container(settings.MAILPILE_DOCKER_IMAGE,
-        #                                        name=container_id,
-        #                                       )
-        # c.start(container, port_bindings={1111: port})
+        container = c.create_container(settings.MAILPILE_DOCKER_IMAGE,
+                                       name=container_id,
+        )
+        c.start(container, port_bindings={1111: port})
         print "Creating nginx configuration for mailpile container"
         template = settings.JINJA_ENV.get_template("user-app.conf.tpl")
         configuration = template.render(path="mailpile/" + username, port=port)
