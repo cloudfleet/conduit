906cae6080224818d0bfd61544d60cb930f39d69
DoubleMalt <christoph@web.crofting.com>
2014-10-08 09:51:26 +0200

    added nginx configuration to be able to test mailpile

--- Vagrantfile
+++ Vagrantfile
@@ -3,5 +3,6 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "trusty64"
   config.vm.network "forwarded_port", guest: 5000, host: 5000
+  config.vm.network "forwarded_port", guest: 8888, host: 8888
   config.vm.provision "shell", path: 'scripts/bootstrap-vagrant.sh'
 end
--- scripts/bootstrap-vagrant.sh
+++ scripts/bootstrap-vagrant.sh
@@ -4,6 +4,8 @@ apt-get update && \
 apt-get upgrade -y && \
 apt-get install python-pip docker.io nginx -y && \
 (cd /vagrant && pip install -r requirements.txt) && \
+cp /vagrant/scripts/nginx.conf /etc/nginx/sites-enabled/mailpile && \
+service nginx restart && \
 adduser vagrant docker && \
 mkdir -p /opt/cloudfleet && \
 chown vagrant /opt/cloudfleet
