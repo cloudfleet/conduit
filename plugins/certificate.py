f7ea74a3930f30ff2f87c6cbd25a1a9e1f529ddf
Christoph Witzany <christoph@web.crofting.com>
2015-06-17 13:35:42 +0200

    create cert plugin

---
+++ plugins/certificate.py
@@ -0,0 +1,12 @@
+import requests
+import shutil
+
+def handle(event):
+    if event.get('status') == 'is_signed':
+      r = requests.get("https://spire.cloudfleet.io/dashboard/blimp/api/get_cert")
+      
+      if r.status_code == 200:
+        with open("/opt/cloudfleet/data/shared/tls/tls_crt.pem.tmp", "w") as fh:
+          fh.write(r.text)
+        shutil.move("/opt/cloudfleet/data/shared/tls/tls_crt.pem.tmp", "/opt/cloudfleet/data/shared/tls/tls_crt.pem")
+          
