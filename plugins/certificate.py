9ab6e80b1c12c7c301f8e050a65d32d00ade9382
Christoph Witzany <christoph@web.crofting.com>
2015-07-14 18:19:12 +0200

    change way how to import settings

--- plugins/certificate.py
+++ plugins/certificate.py
@@ -1,13 +1,12 @@
 import requests
 import shutil
 import sys
-
-from settings import CLOUDFLEET_SECRET, CLOUDFLEET_DOMAIN
+import settings
 
 def handle(event):
     if event.get('status') == 'is_signed':
       sys.stdout.write("Retrieving certificate ...\n")
-      r = requests.post("https://spire.cloudfleet.io/dashboard/blimp/api/get_cert", {'domain': CLOUDFLEET_DOMAIN, 'secret': CLOUDFLEET_SECRET})
+      r = requests.post("https://spire.cloudfleet.io/dashboard/blimp/api/get_cert", {'domain': settings.CLOUDFLEET_DOMAIN, 'secret': settings.CLOUDFLEET_SECRET})
       sys.stdout.write("Response: %s" % r.text)
       if r.status_code == 200:
         with open("/opt/cloudfleet/data/shared/tls/tls_crt.pem.tmp", "w") as fh:
