a3bc22d68eaf345d4388eec52fa6cc5418e28590
Christoph Witzany <christoph@web.crofting.com>
2015-07-14 18:02:56 +0200

    add debugging and authentication

--- plugins/certificate.py
+++ plugins/certificate.py
@@ -1,11 +1,15 @@
 import requests
 import shutil
+import sys
+
+from settings import CLOUDFLEET_SECRET, CLOUDFLEET_DOMAIN
 
 def handle(event):
     if event.get('status') == 'is_signed':
-      r = requests.get("https://spire.cloudfleet.io/dashboard/blimp/api/get_cert")
-
+      sys.stdout.write("Retrieving certificate ...\n")
+      r = requests.post("https://spire.cloudfleet.io/dashboard/blimp/api/get_cert", {'domain': CLOUDFLEET_DOMAIN, 'secret': CLOUDFLEET_SECRET})
+      sys.stdout.write("Response: %s" % r.text)
       if r.status_code == 200:
         with open("/opt/cloudfleet/data/shared/tls/tls_crt.pem.tmp", "w") as fh:
-          fh.write(r.text)
+          fh.write(r.json.cert)
         shutil.move("/opt/cloudfleet/data/shared/tls/tls_crt.pem.tmp", "/opt/cloudfleet/data/shared/tls/tls_crt.pem")
--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ channels = {
     "users": [plugins.users.handle],
     "certificate": [plugins.certificate.handle]
 }
+
+CLOUDFLEET_SECRET = os.getEnv("CLOUDFLEET_SECRET")
+CLOUDFLEET_DOMAIN = os.getEnv("CLOUDFLEET_DOMAIN")
